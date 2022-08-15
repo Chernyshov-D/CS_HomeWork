0ba847dfb3a8076aa9cdfe700e0d88247cf64ba7
Chernyshov-D <imchd@yandex.ru>
2022-08-15 17:23:48 +0400

    Task2

--- Lesson2/Task2/Program.cs
+++ Lesson2/Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает вторую цифру этого числа.
+
+System.Console.Write("Введите трехзначное число: ");
+string input = Console.ReadLine();
+int num = int.Parse(input);
+
+if(num >= 100 && num < 1000) {
+    int one = num / 100 % 10;
+    int two = num / 10 % 10;
+    int three = num % 10;
+    int result = one * 10 + three;
+    System.Console.WriteLine(result); 
+} else {
+    System.Console.WriteLine("Число не трехзначное!");
+}
