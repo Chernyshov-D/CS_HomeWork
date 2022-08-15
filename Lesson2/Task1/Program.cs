9942c07ceaec46f401c9b1b37dfcab2cc5765a15
Chernyshov-D <imchd@yandex.ru>
2022-08-15 16:57:02 +0400

    Task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Prompt(string message) {
+    System.Console.WriteLine(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int value = Prompt("Введите трехзначное число: ");
+if (value >= 100 && value < 1000) {
+int num = value / 10 % 10;
+System.Console.WriteLine(num);
+} else {
+    System.Console.WriteLine("Число не трехзначное!");
+}
