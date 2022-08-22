69ac8dedcf5b468e4f3fef7fc0c4b12ea6f19e1c
Chernyshov-D <imchd@yandex.ru>
2022-08-22 11:28:16 +0400

    Task1

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и
+// возводит число A в натуральную степень B.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+double num(int a, int b)
+{   
+    double result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+int a = Prompt("Введите первое число: ");
+int b = Prompt("Введите второе число: ");
+double exponentiation = num(a, b);
+
+System.Console.Write($"Результат: {exponentiation}");
