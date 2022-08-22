03fb647a8dae090c46de178a173c8144b5e173fb
Chernyshov-D <imchd@yandex.ru>
2022-08-22 11:41:43 +0400

    Task2

--- Lesson4/Task1/Program.cs
+++ Lesson4/Task1/Program.cs
@@ -1,19 +1,16 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и
 // возводит число A в натуральную степень B.
 
-int Prompt(string message)
-{
+int Prompt(string message) {
     System.Console.Write(message);
     string input = Console.ReadLine();
     int result = int.Parse(input);
     return result;
 }
 
-double num(int a, int b)
-{   
+double num(int a, int b) {   
     double result = 1;
-    for (int i = 0; i < b; i++)
-    {
+    for (int i = 0; i < b; i++) {
         result *= a;
     }
     return result;
---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+int Sum(int number)
+{   
+    int result = 0;
+    while (number > 0)
+    {
+        int a = number % 10;
+        result += a;
+        number /= 10;
+    }
+    return result;
+}
+
+int num = Prompt("Введите число: ");
+int totalSum = Sum(num);
+System.Console.Write($"Сумма цифр в числе: {totalSum}");
