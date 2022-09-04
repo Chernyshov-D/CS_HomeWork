da3132d71b064677afb17b1fc975192cb44796cd
Chernyshov-D <imchd@yandex.ru>
2022-09-04 08:08:56 +0400

    Task1

---
+++ Lesson9/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+void FindPositiveNumbers(int m, int n)
+{
+    if (m <= n) 
+    {
+        if (m % 2 == 0) System.Console.Write($"{m} ");
+        FindPositiveNumbers(m + 1, n);
+    }
+
+}
+
+int m = Prompt("Введите первое число: ");
+int n = Prompt("Введите второе число: ");
+FindPositiveNumbers(m, n);
